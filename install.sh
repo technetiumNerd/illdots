8f404db090628e16c871c9eb856ddc53f6e679e4
Philip Olesen <nb.philip@gmail.com>
2021-01-24 16:31:54 +0100

    if statement one-liner

--- install.sh
+++ install.sh
@@ -7,18 +7,10 @@ VRCPATH=$HOME"/.vimrc"
 
 # Tell user to have the things
 echo "Make sure you have vim, zsh, and oh-my-zsh installed"
-if [ ! -d $OMZPATH ];
-then
-    echo "No OMZ installed? Bye!"
-    exit 1
-fi
+[ ! -d $OMZPATH ] && echo "No OMZ installed? Bye!" && exit 1;
 
 # User needs symlinks to repo
 echo "Adding symbolic links for theme and vimrc"
-<<<<<<< HEAD
-=======
-
->>>>>>> fac1422 (?)
 rm -f "${OMZPATH}/custom/themes/illtheme.zsh-theme" "${VRCPATH}"
 ln -s $SCDIR/illtheme.zsh-theme "${OMZPATH}/custom/themes/"
 ln -s $SCDIR/vimrc $VRCPATH
