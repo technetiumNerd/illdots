8ab24e6056cae971cb9ae7f66ec4cfcf2b655597
Philip Olesen <nb.philip@gmail.com>
2021-01-24 15:22:14 +0100

    install autochange zshtheme with the SED

--- install.sh
+++ install.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+SCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+OMZPATH=$HOME"/.oh-my-zsh"
+VRCPATH=$HOME"/.vimrc"
+
 echo "Make sure you have vim, zsh, and oh-my-zsh installed"
 
-OMZPATH="~/.oh-my-zsh"
-VRCPATH="~/.vimrc"
+if [ ! -d $OMZPATH ]
+then
+    echo "No OMZ installed? Bye!"
+    exit 1
+fi
 
-rm ${OMZPATH}/custom/themes/illtheme.zsh-theme
-rm ${VRCPATH}
-ln -s illtheme.zsh-theme ${OMZPATH}/custom/themes/
-ln -s vimrc ${VRCPATH}
+echo "Adding symbolic links for theme and vimrc"
 
-sed -i -e 's/robbyrussell/illtheme/' ~/.zshrc
+rm "${OMZPATH}/custom/themes/illtheme.zsh-theme" "${VRCPATH}"
+ln -s $SCDIR/illtheme.zsh-theme "${OMZPATH}/custom/themes/"
+ln -s $SCDIR/vimrc $VRCPATH
+
+echo "Changing theme in .zshrc"
+
+sed -i -E 's/^ZSH_THEME=".+?"$/ZSH_THEME="illtheme"/g' $HOME"/.zshrc"
+
+echo "All done!"
