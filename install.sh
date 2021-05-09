434c7bc3a6256555747954d9566b049d95b5259e
Philip Olesen <nb.philip@gmail.com>
2021-05-09 15:17:42 +0200

    Install.sh checks for shit, installs gruvbox

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Install gruvbox the vim way?
+#GRUVINSTALL=false
+GRUVINSTALL=true
 
 # Do initial paths
 SCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -7,9 +10,11 @@ VRCPATH=$HOME"/.vimrc"
 
 # Tell user to have the things
 echo "Make sure you have vim, zsh, and oh-my-zsh installed"
-[ ! -d $OMZPATH ] && echo "No OMZ installed? Bye!" && exit 1;
-[ ! $(command -v zsh) ] && echo "No ZSH installed? Bye!" && exit 1;
-[ ! $(command -v vim) ] && echo "No VIM installed? Bye!" && exit 1;
+DOEXIT=false;
+[ ! -d $OMZPATH ] && echo "No OMZ installed?" && DOEXIT=true;
+[ ! $(command -v zsh) ] && echo "No ZSH installed?" && DOEXIT=true;
+[ ! $(command -v vim) ] && echo "No VIM installed?" && DOEXIT=true;
+[ $DOEXIT ] && echo "Bye!" && exit 1;
 
 # User needs symlinks to repo
 echo "Adding symbolic links for theme and vimrc"
@@ -19,9 +24,12 @@ ln -s "$SCDIR/alias" "${OMZPATH}/custom/plugins/"
 [ -f ~/.vimrc ] && cp $VRCPATH "$HOME/.vimrc.bak" && echo ".vimrc backed up to .vimrc.bak";
 ln -s "$SCDIR/vimrc" $VRCPATH
 
+# User should have gruvbox for vim; installing with no pluginmanager
+[ ! -d "~/.vim/pack/default/autostart/gruvbox/" ] && mkdir -p "~/.vim/pack/default/start" && git clone https://github.com/morhetz/gruvbox.git "~/.vim/pack/default/start/gruvbox";
 
 # User should use the theme
-echo "Changing theme in .zshrc"
+echo "Backing up .zshrc to .zshrc.bak and changing theme"
+[ -f ~/.zshrc ] && cp "~/.zshrc" "~/.vimrc.bak" && echo ".zshrc backed up to .zshrc.bak";
 sed -i -E 's/^ZSH_THEME=".+?"$/ZSH_THEME="illtheme"/g' $HOME"/.zshrc"
 
 # Add plugins zsh-syntax-highlighting, zsh-autosuggestions, and my aliases
