7d4ab16e414d2ca0168e00b69f11288268e12f21
Philip Olesen <nb.philip@gmail.com>
2021-08-24 17:19:42 +0200

    namedir

--- alias/alias.plugin.zsh
+++ alias/alias.plugin.zsh
@@ -2,3 +2,5 @@ alias ncmpcpp="ncmpcpp -q"
 alias ealias="vim ~/.oh-my-zsh/custom/plugins/alias/alias.plugin.zsh"
 alias ezsh="vim ~/.zshrc"
 alias evim="vim ~/.vimrc"
+
+namedir () { $1=$PWD ; : ~$1 }
