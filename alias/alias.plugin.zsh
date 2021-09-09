d8edf04f6117af395dc9e910bbe90c5364ad7a06
Philip Olesen <nb.philip@gmail.com>
2021-09-09 11:25:18 +0200

    remove 'namedir'

--- alias/alias.plugin.zsh
+++ alias/alias.plugin.zsh
@@ -2,5 +2,3 @@ alias ncmpcpp="ncmpcpp -q"
 alias ealias="vim ~/.oh-my-zsh/custom/plugins/alias/alias.plugin.zsh"
 alias ezsh="vim ~/.zshrc"
 alias evim="vim ~/.vimrc"
-
-namedir () { $1=$PWD ; : ~$1 }
